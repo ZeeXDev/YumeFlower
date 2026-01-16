c6c1175f9c91a97fc35115f40ad9cb7f7e866f5a
ZeeXDev <otakukingcey@gmail.com>
2026-01-16 09:06:25 +0000

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/BubleWatch'
 AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1003182880183 -1002748899284 -1003025652660") # add multiple channels here, separated by single space
 AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '-100')
 
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-100'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
@@ -108,7 +108,7 @@ PM_SEARCH = bool(environ.get('PM_SEARCH', False))
 EMOJI_MODE = bool(environ.get('EMOJI_MODE', True)) 
 PAID_STREAM = bool(environ.get('PAID_STREAM', False)) 
 STREAM_MODE = bool(environ.get('STREAM_MODE', True))
-MAINTENANCE_MODE = bool(environ.get('MAINTENANCE_MODE', False)) 
+MAINTENANCE_MODE = bool(environ.get('MAINTENANCE_MODE', True) 
 
 
 IGNORE_WORDS = (list(os.environ.get("IGNORE_WORDS").split(",")) if os.environ.get("IGNORE_WORDS") else []) #Remove Words While Searching Files
